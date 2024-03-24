a42cc7811bcf57d555334c1514b247c14b2c3771
Karan <aukzblur@gmail.com>
2024-03-24 14:05:49 -0400

    added circle with key input movement w/ bug fixes

--- snakegame.py
+++ snakegame.py
@@ -16,23 +16,48 @@ Extra:
     Database to store login information (per user)
     Each login will save high score
 '''
+class Ball():
+    def __init__(self) -> None:
+        pass
 
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
+    def Collisions(self):
+        pass
+
 
 def main(): # the game will go here
+    pygame.init()
+    screen = pygame.display.set_mode((1280, 720))
+    clock = pygame.time.Clock()
+    running = True
+    dt = 0 # delta time
+
+    player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+
     while running:
-        for event in pygame.even.get():
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-    screen.fill("purple")
+        screen.fill("purple") # draws screen color
+        pygame.draw.circle(screen, "red", player_pos, 40) # where to draw, color, where on screen, radius
 
-    pygame.display.flip()
+        keys = pygame.key.get_pressed()
 
-    clock.tick(60)
+        if keys[pygame.K_w]:
+            player_pos.y -= 300 * dt
+        if keys[pygame.K_s]:
+            player_pos.y += 300 * dt
+        if keys[pygame.K_a]:
+            player_pos.x -= 300 * dt
+        if keys[pygame.K_d]:
+            player_pos.x += 300 * dt
+
+
+        pygame.display.flip()
+
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__": # makes sure that the main() function is not imported 
